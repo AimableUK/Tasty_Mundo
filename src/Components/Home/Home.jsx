38f49156c2b23ede2c89cd948f664e4375d3ab45
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-07-18 09:14:11 +0200

    Worked with the Testimonial section, new from Tistii Section

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -14,6 +14,8 @@ import chickenMeatImg from "../../assets/chickenMeat.webp";
 import appleImg from "../../assets/apple.webp";
 import cucumberImg from "../../assets/cucumber.webp";
 import quinoaImg from "../../assets/quinoa.webp";
+import speedymassamanbeefstirfryImg from "../../assets/speedymassamanbeefstirfry.jpg";
+import testimonialImg from "../../assets/testimonial.jpg";
 import CosmosCanvas from "../canvas/CosmosCanvas";
 
 const Home = () => {
@@ -403,28 +405,229 @@ const Home = () => {
       </section>
 
       {/* New From TastyMundo */}
-      <section>
-        <h1></h1>
-        <div></div>
+      <section className="p-3 md:px-10 bg-primaryBody text-white">
+        <h1 className="font-roboto text-xl md:text-2xl font-semibold">
+          New from Tasty Mundo
+        </h1>
+        {/* flex flex-col md:flex-row */}
+        <div className="my-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-y-4">
+          <div className="rounded-md main text-start">
+            <img
+              src={speedymassamanbeefstirfryImg}
+              loading="lazy"
+              alt="speedy massaman beef stir-fry"
+              className="w-fit rounded-t-md"
+            />
+            <div className="p-3">
+              <h3 className="font-roboto font-semibold">
+                Speedy massaman beef stir-fry
+              </h3>
+              <p className="text-start text-sm text-gray-300">
+                Fast and tasty, this dish captures the same spiced, creamy
+                coconut flavours of a curry.
+              </p>
+              <button className="mt-2 border p-1 w-full rounded-md border-gray-400 hover:bg-primaryColor hover:border-primaryBody transform transition-all duration-200 ease-in-out font-semibold active:scale-95">
+                View Recipe
+              </button>
+            </div>
+          </div>
+          <div className="rounded-md main text-start">
+            <img
+              src={speedymassamanbeefstirfryImg}
+              loading="lazy"
+              alt="speedy massaman beef stir-fry"
+              className="w-fit rounded-t-md"
+            />
+            <div className="p-3">
+              <h3 className="font-roboto font-semibold">
+                Speedy massaman beef stir-fry
+              </h3>
+              <p className="text-start text-sm text-gray-300">
+                Fast and tasty, this dish captures the same spiced, creamy
+                coconut flavours of a curry.
+              </p>
+              <button className="mt-2 border p-1 w-full rounded-md border-gray-400 hover:bg-primaryColor hover:border-primaryBody transform transition-all duration-200 ease-in-out font-semibold active:scale-95">
+                View Recipe
+              </button>
+            </div>
+          </div>
+          <div className="rounded-md main text-start">
+            <img
+              src={speedymassamanbeefstirfryImg}
+              loading="lazy"
+              alt="speedy massaman beef stir-fry"
+              className="w-fit rounded-t-md"
+            />
+            <div className="p-3">
+              <h3 className="font-roboto font-semibold">
+                Speedy massaman beef stir-fry
+              </h3>
+              <p className="text-start text-sm text-gray-300">
+                Fast and tasty, this dish captures the same spiced, creamy
+                coconut flavours of a curry.
+              </p>
+              <button className="mt-2 border p-1 w-full rounded-md border-gray-400 hover:bg-primaryColor hover:border-primaryBody transform transition-all duration-200 ease-in-out font-semibold active:scale-95">
+                View Recipe
+              </button>
+            </div>
+          </div>
+          <div className="rounded-md main text-start">
+            <img
+              src={speedymassamanbeefstirfryImg}
+              loading="lazy"
+              alt="speedy massaman beef stir-fry"
+              className="w-fit rounded-t-md"
+            />
+            <div className="p-3">
+              <h3 className="font-roboto font-semibold">
+                Speedy massaman beef stir-fry
+              </h3>
+              <p className="text-start text-sm text-gray-300">
+                Fast and tasty, this dish captures the same spiced, creamy
+                coconut flavours of a curry.
+              </p>
+              <button className="mt-2 border p-1 w-full rounded-md border-gray-400 hover:bg-primaryColor hover:border-primaryBody transform transition-all duration-200 ease-in-out font-semibold active:scale-95">
+                View Recipe
+              </button>
+            </div>
+          </div>
+        </div>
       </section>
 
       {/* Testimonial */}
-      <section>
+      <section className="p-3 md:px-10 bg-primaryBody text-white flex flex-col font-nunito md:grid grid-cols-3">
         {/* Picture */}
-        <div></div>
+        <div className="col-span-1">
+          <img
+            src={testimonialImg}
+            loading="lazy"
+            alt="testimonial-Img-1"
+            className="size-64 rounded-md"
+          />
+        </div>
 
         {/* Details */}
-        <div>
+        <div className="col-span-2 self-end flex flex-col gap-y-6">
           {/* stars */}
-          <div></div>
+          <div className="flex flex-row">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="#F6BB23"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6 text-amber-400"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="#F6BB23"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6 text-amber-400"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="#F6BB23"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6 text-amber-400"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="#F6BB23"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6 text-amber-400"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="#F6BB23"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6 text-amber-400"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+              />
+            </svg>
+          </div>
 
           {/* Testimonial */}
-          <div></div>
+          <p>
+            "I absolutely love how easy it is to use this site! I just type in
+            the ingredients I have at home, and within seconds, it suggests
+            recipes I'd never think of on my own. It's like having a personal
+            chef that understands my kitchen. The meals are quick, creative, and
+            actually delicious!"
+          </p>
 
-          {/* Name + Other */}
-          <div>
-            <div></div>
-            <div></div>
+          <div className="flex flex-col md:flex-row justify-between">
+            {/* Name + Other */}
+            <div className="flex flex-col font-roboto">
+              <h3 className="font-bold">Oliva, S.</h3>
+              <h4 className="text-gray-300">Chef at Mariot</h4>
+            </div>
+            <div className="flex flex-row self-end gap-x-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={2}
+                stroke="currentColor"
+                className="size-6 bg-primaryColor rounded-full cursor-pointer active:scale-90"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m11.25 9-3 3m0 0 3 3m-3-3h7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                />
+              </svg>
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={2}
+                stroke="currentColor"
+                className="size-6 bg-primaryColor rounded-full cursor-pointer active:scale-90"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                />
+              </svg>
+            </div>
           </div>
         </div>
       </section>
