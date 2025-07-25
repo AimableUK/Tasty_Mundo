8061ec2507058a9391062ffdcb2c5608bc521544
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-07-25 09:59:01 +0200

    Changed The SVG Usage for good Schematic Syntax

--- src/Components/MainChat/ChatPage/ChatSideBar/ChatSideBar.jsx
+++ src/Components/MainChat/ChatPage/ChatSideBar/ChatSideBar.jsx
@@ -14,46 +14,23 @@ const ChatSideBar = ({ tastyMundoBW, setSavedChats }) => {
         </div>
 
         {/* Navigation */}
-        <div className="flex flex-col gap-y-2">
+        <div className="flex flex-col gap-y-2 items-center">
           {/* New Chat */}
           <div className="relative flex flex-row items-center group">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.4}
-              stroke="currentColor"
-              className="size-8 cursor-pointer text-gray-300 hover:bg-gray-700 active:bg-inherit transform duration-100 ease-in-out rounded-md p-1"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
-              />
-            </svg>
+            <i className="bx  bx-edit-alt bx-sm cursor-pointer text-gray-300 hover:bg-gray-700
+                active:bg-inherit transform duration-100 ease-in-out p-1 rounded-md" ></i> 
             <p className="hidden group-hover:block text-sm absolute whitespace-nowrap font-semibold -right-28 border border-gray-800 px-3 rounded-md bg-gray-900">
               New Chat
             </p>
           </div>
-
-          {/* Search in history */}
+          {/* Ingredient Ideas */}
           <div className="relative flex flex-row items-center group">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.4}
-              stroke="currentColor"
-              className="size-8 cursor-pointer text-gray-300 hover:bg-gray-700 active:bg-inherit transform duration-100 ease-in-out rounded-md p-1"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
-              />
-            </svg>
-            <p className="hidden group-hover:block text-sm absolute whitespace-nowrap font-semibold -right-32 border border-gray-800 px-3 rounded-md bg-gray-900">
-              Search Chats
+            <i
+              className="bx bx-sm bx-burger-alt cursor-pointer text-gray-300 hover:bg-gray-700
+                active:bg-inherit transform duration-100 ease-in-out rounded-md p-1"
+            ></i>
+            <p className="hidden group-hover:block text-sm absolute whitespace-nowrap font-semibold -right-28 border border-gray-800 px-3 rounded-md bg-gray-900">
+              Get Ideas
             </p>
           </div>
           {/* History */}
